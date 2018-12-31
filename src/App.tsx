252c725c656c2eb8f2d66707ca835902cfce2579
Jason G <jason@jroid.com>
2018-12-30 20:53:08 -0500

    initial bootstraped app to talk to the backend service

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,81 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+const TITLE =  'Ballot Time'
+
+const axiosBackEndGraphQL = axios.create({
+  baseURL: 'http://127.0.0.1:3001/graphql',
+});
+
+const GET_CATEGORIES = `
+{
+  categories2 {
+    categoryId
+    displayName
+    displayContent
+  }
+}`;
+
+type ICategory = {  
+  dislayName: string;
+  displayContent: string;
+  categoryId: number;  
+}[]
 
 class App extends Component {
+  state = {
+    categories: [{
+      displayName: "",
+      displayContent: "",
+      categoryId: 0,
+    }],
+    errors: null,
+  }
   render() {
+    // get the state
+    const { categories } = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>{TITLE}</h1>
+          {categories.map(category => (
+          //  <Category category={displayName} />
+          <h4 key={category.categoryId}>{category.displayName}</h4>
+          ))}
         </header>
       </div>
     );
   }
+
+
+
+  componentDidMount() {
+    this.onFetchFromBackEnd();
+  }
+
+  onFetchFromBackEnd = () => {
+    axiosBackEndGraphQL
+      .post('', { query: GET_CATEGORIES })
+      .then(result => {
+          console.log(result.data.data.categories2);
+          this.setState(() => ({
+            categories: result.data.data.categories2,
+            errors: result.data.errors,
+          }));
+        }
+      );
+  };
 }
 
+const Category = ({ category = {} }) => (
+  <div>
+    <p>
+      <strong>Category: {category}</strong>
+    </p>
+  </div>
+);
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,47 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
+import gql from "graphql-tag";
+
+/*
+const client = new ApolloClient({
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.map(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+          ),
+        );
+      if (networkError) console.log(`[Network error]: ${networkError}`);
+    }),
+    new HttpLink({
+      uri: 'http://localhost:3001/graphql'
+    })
+  ]),
+  cache: new InMemoryCache()
+})
+
+client.query(
+    {
+        query: gql`
+          query GetLaunch {
+            launch(id: 56) {
+              id
+              mission {
+                name
+              }
+            }
+          }
+        `
+      }   
+);
+*/
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
